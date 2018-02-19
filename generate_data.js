92beb02ab34ecff239a6505f97dcfae1c3c2c949
vaibhav <da-vaibhav@users.noreply.github.com>
2018-02-20 00:59:48 +0530

    mock data generator

--- generate_data.js
+++ generate_data.js
@@ -1,13 +1,30 @@
 const _ = require('lodash');
 const faker = require('faker');
 
+const statuses = [
+  'Pending Approval',
+  'Draft',
+  'Purchased',
+  'Parttially Received',
+  'Received',
+  'Rejected',
+  'Closed'
+];
+
 function Generate_Data () {
-  const object = { po: [] };
-  _.times(100, (n) => {
-    object["po"].push({
-      id: faker.random.uuid()
-    });
-  });
+  const object = {  };
+
+  const arr = _.times(100, (n) =>
+    ({ id: faker.random.uuid(),
+       name: faker.name.firstName(),
+       vendor: faker.company.companyName(),
+       orderNumber: faker.random.alphaNumeric(5),
+       date: faker.date.past(),
+       status: _.sample(statuses)
+    })
+  );
+
+  object["po"] = arr;
 
   return object;
 };
