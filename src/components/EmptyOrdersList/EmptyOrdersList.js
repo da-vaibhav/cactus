548b00cf10937db82dd062893657893b16c457f1
vaibhav <da-vaibhav@users.noreply.github.com>
2018-02-25 21:43:01 +0530

    Empty Orders List component

---
+++ src/components/EmptyOrdersList/EmptyOrdersList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EmptyOrdersList extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default EmptyOrdersList;
