885fe6dc17b1afbdd8feb92c1b6a8f14fd1526e5
vaibhav <da-vaibhav@users.noreply.github.com>
2018-02-25 23:50:23 +0530

    order details

--- src/components/OrdersList/OrdersList.js
+++ src/components/OrdersList/OrdersList.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './OrdersList.css';
 
 class OrdersList extends Component {
   render() {
     const orders = this.props;
+    const baseClassName = 'po_list__item';
     return (
-      <div>
-        {orders.orders.map((order, i) =>
-          <div key={i}>
+      orders.orders.map((order, i) =>
+        <div key={i} className={ `${baseClassName}` }>
+          <span className={` `}>
+            <input type="checkbox"/>
+          </span>
+
+          <span className={ `${baseClassName}__link` }>
             <Link to='/'>{order.orderNumber}</Link>
+          </span>
+
+          <span className={`${baseClassName}__date`}>
             <time>{order.date}</time>
-            {order.vendor}
+          </span>
+
+          <span className={`${baseClassName}__vendor`}>{order.vendor}</span>
+          <span
+            className={ `po_status ${baseClassName}_status__${order.status.replace(/\s/g, "")}` }>
             {order.status}
-            {order.name}
+          </span>
+
+          <span className={`${baseClassName}__expectedDeliveryDate`}>
             <time>{order.expectedDeliveryDate}</time>
-            {order.amount}
-          </div>
-        )}
-      </div>
+          </span>
+
+          <span className={ `${baseClassName}__amount` }>&#8377;{order.amount}</span>
+        </div>
+      )
     );
   }
 }
