e5a8fc84762a7256822d0b60cb75ad49e6b5ecf7
vaibhav <da-vaibhav@users.noreply.github.com>
2018-02-25 21:40:46 +0530

    cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
 class Home extends Component {
-
-  componentWillMount = () => {
-    fetch('http://localhost:4000/po')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
-  }
-
   render() {
     return (
       <div>
-        <p>Home</p>
-        Please <a href="login">login</a>
+        <h3> home </h3>
       </div>
     );
   }
