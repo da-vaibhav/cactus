f9fe8bffe219ea43aa3b925d327ebacdc627d899
vaibhav <da-vaibhav@users.noreply.github.com>
2018-02-25 21:43:19 +0530

    app page js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
-import Login from './Login';
-
+import Sidebar  from './components/Sidebar/Sidebar';
+import routes from './routes';
+import Search from './components/Search/Search'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div className="App">
-          <h3>welcome!</h3>
+          <Search />
           <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
+          <div>
+            <nav><Sidebar /></nav>
+            <main>
+              {routes.map(route =>
+                <Route
+                  key={route.title}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.component}
+                />
+              )}
+            </main>
+          </div>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
