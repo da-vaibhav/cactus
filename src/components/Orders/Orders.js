ac8856fd4b1e8755e0271657a79c1a363da4b0cf
vaibhav <da-vaibhav@users.noreply.github.com>
2018-02-25 23:50:05 +0530

    list headers

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -20,9 +20,22 @@ class Orders extends Component {
   render() {
     const { orders } = this.state;
     const hasOrders = orders.length;
+    const baseClassName = 'po_list__container';
 
     return (
-      <div>
+      <div className={ hasOrders ? baseClassName : ''} >
+        <div className={ 'po_list__header__container' } >
+          <span className={ `${baseClassName}_header_` }><input type="checkbox"/></span>
+          <span className={ `${baseClassName}_header__Order` }>Order #</span>
+          <span className={ `${baseClassName}_header_Date` }>Date </span>
+          <span className={ `${baseClassName}_header_Vendor` }>Vendor </span>
+          <span className={ `${baseClassName}_header_Status` }>Status</span>
+          <span
+            className={ `${baseClassName}_header_ExpectedDeliveryDate` }>
+            Expected Delivery Date
+          </span>
+          <span className={ `${baseClassName}_header_Amount` }>Amount</span>
+        </div>
         {
           hasOrders ? <OrdersList orders={ orders } /> : <EmptyOrdersList />
         }
